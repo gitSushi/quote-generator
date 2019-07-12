f38bede79d82941ee2c07f9f4b45d77c464fc7a8
gitSushi <lossushi37@gmail.com>
2019-07-12 17:18:30 +0200

    Updates

---
+++ precache-manifest.40b1bd8763ed19aec1b43fe079899bab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f453449146b2d7279b8e6ce9080739e5",
+    "url": "/quote-generator/index.html"
+  },
+  {
+    "revision": "ddc20b9fa19df63a8425",
+    "url": "/quote-generator/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/quote-generator/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ddc20b9fa19df63a8425",
+    "url": "/quote-generator/static/js/main.51248a8f.chunk.js"
+  },
+  {
+    "revision": "b42996cbb3818e11c29d",
+    "url": "/quote-generator/static/js/runtime~main.4226e67e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quote-generator/static/media/logo.5d5d9eef.svg"
+  }
+]);
